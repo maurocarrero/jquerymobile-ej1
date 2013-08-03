51917333ac800aa86979f95d36ec329ebc13830f
Mauro Carrero <maurocarrero@lataperainvisible.com.uy>
2013-08-03 14:22:58 -0300

    Add html and js

---
+++ script.js
@@ -0,0 +1,26 @@
+
+$(document).ready(function(){
+	var clientes = cargarDatos();
+
+	for (var i=0; i < clientes.length; i++) {
+		$("ul#clientes").append("<li><a href='#pag2?id='" + i + "'>" + clientes[i] + "</a></li>");
+	}
+	
+	$("ul#clientes").listview("refresh");
+	
+});
+
+
+function cargarDatos()
+{
+	var clientes = new Array();
+
+	clientes[0] = "Daniel Bonfiglio";
+	clientes[1] = "Mauro Carrero";
+	clientes[2] = "Pepe Corvina";
+	clientes[3] = "Toto Mendez";
+	clientes[4] = "Valeria Pérez";
+	
+	return clientes;
+}
+
